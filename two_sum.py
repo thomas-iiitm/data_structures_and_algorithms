8f8550e54b890c3f416b08c511fa4f0c0e1fb80c
thomas-iiitm <31181853+thomas-iiitm@users.noreply.github.com>
2025-09-28 14:20:28 +0530

    Update two_sum.py
    updated code to ensure all pairs are captured

--- two_sum.py
+++ two_sum.py
@@ -1,17 +1,17 @@
-# Find pair of numbers that sum up to a number (target). Assume that you have a sorted array (arr)
+# Find ALL pair of numbers that sum up to a number (target). Assume that you have a sorted array (arr)
 # Time: O(n), Space: O(1)
 
-def two_sum(arr, target):
-    left, right = 0, len(arr) - 1
-    
+def two_sum_all(arr, target):
+    left, right = 0, len(arr) - 1 # Using two pointers
+    results = []
     while left < right:
-        current_sum = arr[left] + arr[right]
-        
-        if current_sum == target:
-            return (arr[left], arr[right])  # found the pair
-        elif current_sum < target:
-            left += 1  # need bigger sum
+        s = arr[left] + arr[right]
+        if s == target:
+            results.append((arr[left], arr[right])) # found a pair
+            left += 1
+            right -= 1
+        elif s < target:
+            left += 1   # need bigger sum
         else:
-            right -= 1 # need smaller sum
-    
-    return None
+            right -= 1  # need smaller sum
+    return results or None
